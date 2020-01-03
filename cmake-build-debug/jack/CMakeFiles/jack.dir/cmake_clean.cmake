0633e22ae38cf9d0285c44a770560ca627028f71
tanoak <tanoak10@163.com>
2020-01-03 17:19:14 +0800

    无修改
    Signed-off-by: tanoak <tanoak10@163.com>

---
+++ cmake-build-debug/jack/CMakeFiles/jack.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jack.dir/src/VM.cpp.o"
+  "CMakeFiles/jack.dir/src/jack.cpp.o"
+  "jack"
+  "jack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/jackc/CMakeFiles/jackclib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jackclib.dir/src/Analyzer.cpp.o"
+  "CMakeFiles/jackclib.dir/src/CodeGen.cpp.o"
+  "CMakeFiles/jackclib.dir/src/Error.cpp.o"
+  "CMakeFiles/jackclib.dir/src/Parser.cpp.o"
+  "CMakeFiles/jackclib.dir/src/Scanner.cpp.o"
+  "CMakeFiles/jackclib.dir/src/SymbolTable.cpp.o"
+  "libjackclib.a"
+  "libjackclib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jackclib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
